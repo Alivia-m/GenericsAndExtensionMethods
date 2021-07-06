14efc965edcc9b0bc3fffe6168eff01b0c69a7e0
Alivia-m <aliviagayle@icloud.com>
2021-07-05 23:06:49 -0500

    complete

---
+++ Generics/ExtensionMethods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Generics
+{
+    public static class ExtensionMethods
+    {
+        public static IEnumerable<int> Odds<T>(IEnumerable<int> number)
+        {
+            return number.Where(n => n % 2 != 0);
+        }
+        public static IEnumerable<T> Sort<T>(this IEnumerable<T> collection)
+        {
+            return collection.OrderBy(x => x);
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-
+using System.Collections.Generic;
 namespace Generics
 {
     class Program
@@ -10,25 +10,49 @@ namespace Generics
         //TODO In your class create a property List called "Bin". Set the list type to "T".
         //TODO Create two more classes. One class called "Equipment". The second class called "Food".
         //TODO create two instances of type Storage in the Main method. First Instance will hold type "<Equipment>". The second instance will hold "<food>".
-        //TODO create some instanes of type Food and Equipment and Add them the correct storage bins.
+        //TODO create some instances of type Food and Equipment and Add them the correct storage bins.
 
         //TODO In the program class Create a static void method called CountToNum that has a generic type called "T".
         //TODO Give the method a T paramter called "n".
         //TODO In the method scope make the method print all the numbers from 0 to "n".
         //TODO Call the method in the Main first by giving it a float number. Then call it again by giving it a double number.
 
-
+        public static void CountToNum<T>(T[] n)
+        {
+            foreach(var number in n)
+            {
+                Console.WriteLine(number);
+            }
+        }
 
 
         static void Main(string[] args)
         {
+            Storage<Equipment> equipStorage = new Storage<Equipment>();
+            Storage<Food> foodStorage = new Storage<Food>();
 
-            var numbers = new int[] { 1, 2, 3, 4, 5, 6 };
+            var apples = new Food();
+            var crackers = new Food();
+            var soccerBall = new Equipment();
+            var golfClub = new Equipment();
 
+            foodStorage.Bin.Add(apples);
+            foodStorage.Bin.Add(crackers);
+            equipStorage.Bin.Add(soccerBall);
+            equipStorage.Bin.Add(golfClub);
+          
+            var numbersList1 = new int[] { 1, 2, 3, 4, 5, 6 };
+            var numbersList2 = new double[] { 2.05, 10.3, 17.7, 4.44, 81.0, 6.2};
+            var numbersList3 = new float[] { -1, -2, -3, -4, -5, -6 };
+
+            CountToNum(numbersList2);
+            Console.WriteLine("-----------------");
+            CountToNum(numbersList3);
+
+            Console.WriteLine("-----------------");
+            var sorted = numbersList1.Sort();
+            var odds = ExtensionMethods.Odds<int>(numbersList1);
             
-
-
-
         }
       
 
